9eb336700cf373a9b11c1ec7f8a0fbc37a1ba560
Muzammal Rahim <creative.joomdev@gmail.com>
2020-11-13 18:46:05 +0500

    Floders, Thumbnail, Snippets API

--- cms_project/admin.py
+++ cms_project/admin.py
@@ -1,7 +1,13 @@
 from django_restful_admin import admin
 from .models import ContactUsQuery
 from cms.models import Page, PageType, GlobalPagePermission, StaticPlaceholder
-from djangocms_blog.models import BlogCategory
+from djangocms_blog.models import BlogCategory, Post
+from djangocms_blog.cms_appconfig import BlogConfig
+from djangocms_file.models import Folder
+from djangocms_picture.models import Picture
+from djangocms_snippet.models import Snippet
+from taggit.models import Tag
+from django.contrib.sites.models import Site
 
 admin.site.register(ContactUsQuery)
 
@@ -11,3 +17,10 @@ admin.site.register(PageType)
 admin.site.register(GlobalPagePermission)
 admin.site.register(StaticPlaceholder)
 admin.site.register(BlogCategory)
+admin.site.register(Post)
+admin.site.register(BlogConfig)
+admin.site.register(Folder)
+admin.site.register(Picture)
+admin.site.register(Snippet)
+admin.site.register(Tag)
+admin.site.register(Site)
