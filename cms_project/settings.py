e39da961d2e1fcda7172c4815710e2282df62b07
Creativetech <creative.joomdev@gmail.com>
2020-11-18 17:22:28 +0500

    authentication settings

--- cms_project/settings.py
+++ cms_project/settings.py
@@ -273,11 +273,11 @@ META_USE_SCHEMAORG_PROPERTIES = True  # django-meta 2.x+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated', ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated', ),
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser',
