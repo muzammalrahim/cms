1492c822701d856acf402fba15a7720e0121a284
Arslan Mehmood <arslanmehmood051@gmail.com>
2020-11-23 19:08:09 +0500

    frontend integration

---
+++ frontend/src/app/helper/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const LOGIN_URL = "adm/login/"
+const ME_URL = "adm/users/me"
+
+// export const headers = {
+//     'Content-Type': 'application/json',
+//     // 'Authorization': 'Token 2f2b7961077264b154a140b3111d2063add5f442'
+//   }
+
+export default function login(username, password) {
+    return axios.post(process.env.REACT_APP_API_URL + LOGIN_URL, { username, password })
+}
+// let config ={
+//     header
+// }
+export function me(Authorization) {
+    return axios.get(process.env.REACT_APP_API_URL + ME_URL,  {
+        headers: {
+           Authorization: `Token ${Authorization.token}` ,
+           'Content-Type': 'application/json'
+        }
+  })
+}
