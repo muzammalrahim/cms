837834213da4089d2a39c3cde5d5d32237100d7e
MuhammadRizwanSalik <47275249+mrizwansalik@users.noreply.github.com>
2020-11-25 17:25:46 +0500

    add group and user lists

--- frontend/src/app/main/pages/groups/List.js
+++ frontend/src/app/main/pages/groups/List.js
@@ -1,130 +1,81 @@
+import FusePageCarded from '@fuse/core/FusePageCarded';
+import { makeStyles, fade } from '@material-ui/core/styles';
+import { Button, InputBase } from '@material-ui/core';
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
+import Tables from './Tables'
 
-const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
-];
+const useStyles = makeStyles(theme => ({
+	layoutRoot: {},
+	inputRoot: {
+		color: 'inherit'
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25)
+		}
+	},
+	searchIcon: {
+		padding: theme.spacing(0, 2),
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	inputInput: {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+		paddingRight: `calc(1em + ${theme.spacing(4)}px)`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: '20ch'
+		}
+	}
+}));
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
+function List() {
+	const classes = useStyles();
+
+	return (
+		<FusePageCarded
+			classes={{
+				root: classes.layoutRoot
+			}}
+			header={
+				<div className="py-24">
+					<h4>Groups</h4>
+				</div>
+			}
+			contentToolbar={
+				<div className="px-24">
+					<span>Groups List</span>
+					<InputBase
+            placeholder="Search…"
+            color='primary'
+						classes={{
+							root: classes.inputRoot,
+							input: classes.inputInput
+						}}
+						inputProps={{ 'aria-label': 'search' }}
+					/>
+					<Button variant="contained" color="primary" raised justifyContent="flex-end">
+						Add Group
+					</Button>
+				</div>
+			}
+			content={
+				<div className="p-24">
+					<Tables />
+				</div>
+			}
+		/>
+	);
 }
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
-
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-  },
-  container: {
-    maxHeight: 440,
-  },
-});
-
-export default function StickyHeadTable() {
-  const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
-  return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </Paper>
-  );
-}
+export default List;
--- frontend/src/app/main/pages/users/Add.js
+++ frontend/src/app/main/pages/users/Add.js
@@ -1,28 +1,53 @@
-import { Card, ButtonGroup, Button } from '@material-ui/core';
+import FusePageCarded from '@fuse/core/FusePageCarded';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core'
 import React from 'react';
-import TransferList from './grids';
-// import "./styles.css";
+// import Grid from './grids'
+import Buttons from './Buttons'
 
-export default function App() {
+const useStyles = makeStyles({
+	layoutRoot: {}
+});
+
+function Add() {
+	const classes = useStyles();
+
 	return (
-		<div className="App">
-			<h3>Add group</h3>
-			<Card>
-				<form>
-					<h3>Name:</h3>
-					<input type="text" name="name" />
-					<h6>PERMISSIONS:</h6>
-					<div display="inline">
-						<TransferList />
-					</div>{' '}
-					<hr />
-					<ButtonGroup variant="text" color="" align="right" aria-label="text primary button group">
-						<Button>Save and add another</Button>
-						<Button>Save and continue editing</Button>
-						<Button>Save</Button>
-					</ButtonGroup>
-				</form>
-			</Card>
-		</div>
+		<FusePageCarded
+			classes={{
+				root: classes.layoutRoot
+			}}
+			header={
+				<div className="py-24">
+					<h4>Users</h4>
+				</div>
+			}
+			contentToolbar={
+				<div className="px-24">
+					<h4>Add User</h4>
+				</div>
+			}
+			content={
+				<div className="p-24">
+					<form className={classes.root} autoComplete="off">
+						<TextField id="outlined-basic" label="Name" variant="outlined" fullWidth />
+						<br />
+						<br />
+						<TextField id="outlined-basic" label="Password" variant="outlined" fullWidth />
+						<br />
+						<br />
+						<TextField id="outlined-basic" label="Confirm" variant="outlined" fullWidth />
+						<br />
+						<br />
+						{/* <h4>Permisions:</h4> */}
+						{/* <Grid /> */}
+						<hr />
+						<Buttons />
+					</form>
+				</div>
+			}
+		/>
 	);
 }
+
+export default Add;
